0ed31ad48b0da077eba19a222e90fd5304597dc4
erik-mackie <erik.mackie@gmail.com>
2018-09-03 22:40:06 +0000

    added/ updated packages

---
+++ download_avatars.js
@@ -0,0 +1,13 @@
+
+var images = require("./pullImage");
+
+var args1 = process.argv[2];
+var args2 = process.argv[3];
+
+/*var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+*/
+
+images("lighthouse-labs", "finstagram");
