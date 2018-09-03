a656e3aa7e5b2d3a3bc6663705924b0bbdbd5e1f
erik-mackie <erik.mackie@gmail.com>
2018-09-03 22:45:22 +0000

    request added

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,15 @@
+var request = require('request');
 
-var images = require("./pullImage");
+/*var images = require("./pullImage")*/;
 
 var args1 = process.argv[2];
 var args2 = process.argv[3];
 
-/*var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step5.html'
-};
-*/
 
-images("lighthouse-labs", "finstagram");
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+/*images("lighthouse-labs", "finstagram");*/
+
+
+
+// json parse to get names
