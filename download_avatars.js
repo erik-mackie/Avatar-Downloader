5a87020d291cdd9e7d50d5be4b4097d11f0dc89c
erik-mackie <erik.mackie@gmail.com>
2018-09-04 01:43:54 +0000

    exercise complete

--- download_avatars.js
+++ download_avatars.js
@@ -1,34 +1,38 @@
-var request = require('request');
-const TOKEN = require('./secrets');
-var imageByUrl = require('./downloadImage');
+const request = require('request');
+const secrets = require('./secrets');
+const imageByUrl = require('./downloadImage');
 
-var arg1 = process.argv[2];
-var arg2 = process.argv[3];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': `token ${secrets.GITHUB_TOKEN}`
     }
   };
+  // if missing arguments, return error message
+  if (repoOwner === undefined || repoName === undefined) {
+    console.log("Missing Repo Owner or Name Argument");
+    process.exit();
+  }
   // request to pull data from github
   request(options, (err, res, body) => {
     cb(err, body);
   });
 }
 
-//call back refactor so loop isnt in here
-getRepoContributors(`${arg1}`,`${arg2}`, (err, result) => {
+getRepoContributors(arg1, arg2, (err, results) => {
   console.log("Errors:", err);
   // parse and loop imported string so values are accessable
-  JSON.parse(result).forEach(val => {
-    imageByUrl(val.avatar_url, val.login)
-    /*console.log(`Result: ${val.login}`)*/
+  JSON.parse(results).forEach(result => {
+    imageByUrl(result.avatar_url, result.login);
   });
-})
+});
 
 
-/*node download_avatars.js jquery jquery*/
+
 
